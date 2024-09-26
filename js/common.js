b64ef50826fc1f9fd22a00739f16459a86d0f1b3
nature.beauty.vitae <nature.beauty.vitae@gmail.com>
2024-09-26 14:40:59 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -3,33 +3,7 @@
 function header () {
   with(document) {
        write("<div id='headerMain'>");
-            write("<a class='logo' href='./index.html'>");
-              write("<span class='nav-title'>Nature Beauty Vitae</span>");
-            write("</a>");
-           write("<div class='menuMain'>");
-               write("<ul class='menu'>");
-                    write("<li><a href='index.html'>Home</a>");
-                    write("</li>")
-                    write("<li><a href='call.html'>Call for papers</a>");
-                    write("</li>")
-                    write("<li><a href='program.html'>Program</a>");
-                    write("</li>")
-                    write("<li><a href='practical_info.html'>Practical Information</a>");
-                    write("</li>")
-                    write("<li><a href='mailto:julie.goncharov@uni-goettingen.de'>Contact us</a>");
-                    write("</li>")
-               write("</ul>");
-           write("</div>");
-        write("</div>");
-      write("<div id='project-header'>");
-            write("<image class='project-header-image' src='./assets/images/AIAI_prog.png'/>");
-                write("<div class='main-title'>Nature Beauty Vitae</div>");
-                write("</div>");
-            write("</div>");
-      write("</div>");
-  }
-  imgsearch();
-}
+         
 
 
 
@@ -37,47 +11,3 @@ function footerContainer(){
       with(document){
         write("<div id='footerMain'>");
             write("<div>");
-                    write("<a target='_blank' href='https://uni-goettingen.de/'>");
-                        write("<img src='./assets/images/ugoettingen.png' style='width:120px'>");
-                    write("</a>");
-            write("</div>");
-            write("<div class='project-card-text-container'>");
-                    write("<a class='button' target='_blank' href='https://uni-goettingen.de/'>");
-                        write("University of Göttingen");
-                    write("</a>");
-            write("</div>");
-          write("<div>");
-                  write("<a target='_blank' href='https://www.dfg.de/en'>");
-                      write("<img src='./assets/images/dfg.pnh.png' style='width:100px'>");
-                  write("</a>");
-          write("</div>");
-          write("<div class='project-card-text-container'>");
-                  write("<a class='button' target='_blank' href='https://www.dfg.de/en'>");
-                      write("Deutsche Forschungsgemeinschaft");
-                  write("</a>");
-          write("</div>");
-          write("<div>");
-                  write("<a target='_blank' href='https://vanriemsdijkfoundation.org/'>");
-                      write("<img src='./assets/images/VRF.png' style='width:120px'>");
-                  write("</a>");
-          write("</div>");
-          write("<div class='project-card-text-container'>");
-                  write("<a class='button' target='_blank' href='https://vanriemsdijkfoundation.org/'>");
-                      write("The Van Riemsdijk Foundation");
-                  write("</a>");
-          write("</div>");
-/*ADD UofG, DFG, and possibly VRF
-          write("<div>");
-                  write("<a target='_blank' href='https://ail-workshop.github.io/'>");
-                      write("<img src='./assets/images/ail_logo_blue.png' style='width:120px'>");
-                  write("</a>");
-          write("</div>");
-          write("<div class='project-card-text-container'>");
-                  write("<a class='button' target='_blank' href='https://ail-workshop.github.io/'>");
-                      write("University of Goettingen");
-                  write("</a>");
-          write("</div>");
-*/
-        write("</div>");
-          }
-  }
